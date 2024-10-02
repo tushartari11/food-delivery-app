019e608be19127503215418848d975faa47995c0
Tushar Tari <tushartari11@gmail.com>
2024-10-02 20:30:37 +0200

    Initial Commit

---
+++ restaurant-listing-service/src/main/java/com/telefonica/prepaid/restaurantlisting/controller/RestaurantController.java
@@ -0,0 +1,37 @@
+package com.telefonica.prepaid.restaurantlisting.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import com.telefonica.prepaid.restaurantlisting.dto.RestaurantDTO;
+import com.telefonica.prepaid.restaurantlisting.service.RestaurantService;
+
+@RestController
+@RequestMapping( "/restaurant" )
+@CrossOrigin
+public class RestaurantController {
+
+    @Autowired
+    RestaurantService restaurantService;
+
+    @GetMapping( "/fetchAllRestaurants" )
+    public ResponseEntity<List<RestaurantDTO>> fetchAllRestaurants() {
+        List<RestaurantDTO> allRestaurants = restaurantService.findAllRestaurants();
+        return new ResponseEntity<>( allRestaurants, HttpStatus.OK );
+    }
+
+    @PostMapping( "/addRestaurant" )
+    public ResponseEntity<RestaurantDTO> saveRestaurant( @RequestBody RestaurantDTO restaurantDTO ) {
+        RestaurantDTO restaurantAdded = restaurantService.addRestaurantInDB( restaurantDTO );
+        return new ResponseEntity<>( restaurantAdded, HttpStatus.CREATED );
+    }
+
+    @GetMapping( "fetchById/{id}" )
+    public ResponseEntity<RestaurantDTO> findRestaurantById( @PathVariable Integer id ) {
+        return restaurantService.fetchRestaurantById( id );
+    }
+}
