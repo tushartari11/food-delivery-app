9e1ecee50fed270b553a7fef2f525db0c8a583be
Tushar Tari <tushartari11@gmail.com>
2025-02-27 12:29:24 +0100

    added docker

---
+++ eureka-service/src/test/java/com/rekreation/app/fooddelivery/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rekreation.app.fooddelivery.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
