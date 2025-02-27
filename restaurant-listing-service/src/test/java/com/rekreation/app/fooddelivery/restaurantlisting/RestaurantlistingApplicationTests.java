edf940a57a67aba6bdcd89f131279670c567258b
Tushar Tari <tushartari11@gmail.com>
2025-02-27 14:01:37 +0100

    refactoring changes

--- restaurant-listing-service/src/test/java/com/rekreation/app/fooddelivery/restaurantlisting/RestaurantlistingApplicationTests.java
+++ restaurant-listing-service/src/test/java/com/rekreation/app/fooddelivery/restaurantlisting/RestaurantlistingApplicationTests.java
@@ -1,9 +1,20 @@
 package com.rekreation.app.fooddelivery.restaurantlisting;
 
+import jakarta.ws.rs.core.Application;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@SpringBootTest
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(
+		webEnvironment = SpringBootTest.WebEnvironment.MOCK,
+		classes = Application.class)
+@AutoConfigureMockMvc
+@TestPropertySource(
+		locations = "classpath:application-integrationtest.properties")
 class RestaurantlistingApplicationTests {
 
 	@Test
